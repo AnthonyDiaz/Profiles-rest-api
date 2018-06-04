b7117111e3a6a4017d99104cddb99a1428b716f0
Anthony Diaz <tdiaz@entic.com>
2018-06-04 11:02:05 -0400

    Adding a search feature to the view

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 
-from . import serializers
-from . import models
-from . import permissions
+from . import serializers, models, permissions
+
 
 # Create your views here.
 
@@ -115,3 +113,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
